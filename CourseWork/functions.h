ba982c0556c34698a98ec6a768d2bba2b6c5a183
q <125211365+qwertyabcq@users.noreply.github.com>
2023-06-07 20:04:13 +0300

    Create functions.h

---
+++ CourseWork/functions.h
@@ -0,0 +1,15 @@
+#pragma once
+
+void gotoxy(short x, short y);
+
+void ignore_line();
+
+void initializer(double A, double B, double C);
+
+void menu();
+
+void equation_types();
+
+void calculator();
+
+void exit_prog(bool& execution);
